6b35d8d170a72071a6686b5b3d0c07c441c08563
Omar Muhammad <cds.omarmuhammad92694@alexu.edu.eg>
2024-12-08 21:37:59 +0200

    add delete function

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -1,18 +1,18 @@
-#include <stdio.h>   
-#include <stdlib.h>  
-#include <string.h>  
-struct Student {
-    char  name[10];
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct Student
+{
+    char name[10];
     int id;
     int grades[3];
-
 };
-struct Node {
+struct Node
+{
     struct Student student;
-    struct Node * next;
-    struct Node * prv;
+    struct Node *next;
+    struct Node *prv;
 };
-
 
 struct Student fill_Student()
 {
@@ -30,34 +30,36 @@ struct Student fill_Student()
     return s;
 };
 
-struct Node * Hptr;
-struct Node * Tptr;
+struct Node *Hptr;
+struct Node *Tptr;
 
-struct Node * createNode(struct Student student){
-    struct Node * ptr;
+struct Node *createNode(struct Student student)
+{
+    struct Node *ptr;
 
-    ptr = (struct Node *) malloc(sizeof(struct Node));
-    if(ptr){
+    ptr = (struct Node *)malloc(sizeof(struct Node));
+    if (ptr)
+    {
         ptr->student = student;
         ptr->next = NULL;
         ptr->prv = NULL;
-
     }
     return ptr;
 }
 
-
-
-int addNode (struct Student student){
-    struct Node * ptr;
+int addNode(struct Student student)
+{
+    struct Node *ptr;
     ptr = createNode(student);
     int flag = 0;
-    if(ptr){
+    if (ptr)
+    {
         flag = 1;
-        if(Hptr == NULL) //noList
+        if (Hptr == NULL) // noList
         {
             Hptr = Tptr = ptr;
-        }else
+        }
+        else
         {
 
             Tptr->next = ptr;
@@ -68,87 +70,135 @@ int addNode (struct Student student){
     return flag;
 }
 
+int deleteNode(int loc)
+{
+    struct Node *ptr, *temp;
+    int flag = 0;
 
+    if (Hptr == NULL)
+    {
+        flag = 0;
+        printf("the list is empty");
+        return flag;
+    }
+    else
+    {
+        flag = 1;
 
-
-
-int insertNode(struct Student student,int loc){
-    int flag=0,i=0;
-    struct Node * ptr,*temp;
-    ptr = createNode(student);
-    if(ptr){
-        flag=1;
-        if(Hptr == NULL) //nolist
-        {   
-            Hptr = Tptr = ptr;
-        }else //exits
+        if (loc == 0)
         {
-            if(loc ==0) // first node
+            ptr = Hptr;
+            Hptr = Hptr->next;
+            if (Hptr)
+            { // if there is node
+                Hptr->prv = NULL;
+            }
+            else // if it become empty
+            {
+                Tptr = NULL;
+            }
+            free(ptr);
+        }
+        else
+        {
+            temp = Hptr;
+            for (int i = 0; i < loc - 1 && temp; i++)
+            {
+                temp = temp->next;
+            }
+
+            if (temp == NULL)
+            {
+                flag = 0;
+                printf("Invalid location\n");
+                return flag;
+            }
+            if (temp == Tptr)
+            {
+                Tptr = temp->prv;
+                if (Tptr)
+                {
+
+                    Tptr->next = NULL;
+                }
+            }
+            else
+            {
+                temp->prv->next = temp->next;
+                temp->next->prv = temp->prv;
+            }
+
+            free(temp);
+        }
+    }
+
+    return flag;
+}
+
+int insertNode(struct Student student, int loc)
+{
+    int flag = 0, i = 0;
+    struct Node *ptr, *temp;
+    ptr = createNode(student);
+    if (ptr)
+    {
+        flag = 1;
+        if (Hptr == NULL) // nolist
+        {
+            Hptr = Tptr = ptr;
+        }
+        else // exits
+        {
+            if (loc == 0) // first node
             {
                 ptr->next = Hptr;
                 Hptr->prv = ptr;
-                Hptr = ptr;  
+                Hptr = ptr;
             }
-            else  // middle or last
+            else // middle or last
             {
                 temp = Hptr;
 
-                for (i = 0; i < loc-1 && temp; i++)
+                for (i = 0; i < loc - 1 && temp; i++)
                 {
                     temp = temp->next;
                 }
-                if(temp == Tptr || temp == NULL) //last node
+                if (temp == Tptr || temp == NULL) // last node
                 {
                     ptr->prv = Tptr;
                     Tptr->next = ptr;
-                    Tptr= ptr;
-                }else
+                    Tptr = ptr;
+                }
+                else
                 {
                     temp->next->prv = ptr;
                     ptr->next = temp->next;
                     ptr->prv = temp;
                     temp->next = ptr;
                 }
-                
             }
         }
     }
     return flag;
 }
 
-
-void freeList(){
+void freeList()
+{
     struct Node *ptr;
     while (Hptr)
     {
         ptr = Hptr;
-        Hptr=Hptr->next;
-        free(ptr); 
+        Hptr = Hptr->next;
+        free(ptr);
     }
     Tptr = NULL;
 }
 
-
-struct Student * searchWithName(char * name){
-    struct Node* ptr;
+struct Student *searchWithName(char *name)
+{
+    struct Node *ptr;
     ptr = Hptr;
-    while (strcmp(name,ptr->student.name)!=0 && ptr)
-    {
-        ptr = ptr->next;
-    }
-
-    if(!ptr){
-        return NULL;
-    }
-    return &ptr->student;
-    
-}
-
-
-struct Student * searchWitId(int id){
-      struct Node* ptr;
-    ptr = Hptr;
-    while (id != ptr->student.id&& ptr!=NULL)
+    while (strcmp(name, ptr->student.name) != 0 && ptr)
     {
         ptr = ptr->next;
     }
@@ -157,29 +207,42 @@ struct Student * searchWitId(int id){
     {
         return NULL;
     }
-    
+    return &ptr->student;
+}
 
+struct Student *searchWitId(int id)
+{
+    struct Node *ptr;
+    ptr = Hptr;
+    while (id != ptr->student.id && ptr != NULL)
+    {
+        ptr = ptr->next;
+    }
+
+    if (!ptr)
+    {
+        return NULL;
+    }
 
     return &ptr->student;
 }
 
-
-
-
-
-
-void printAll() {
+void printAll()
+{
     struct Node *ptr = Hptr;
-    if (!ptr) {
+    if (!ptr)
+    {
         printf("The list is empty.\n");
         return;
     }
     printf("Student Details:\n");
-    while (ptr) {
+    while (ptr)
+    {
         printf("Name: %s\n", ptr->student.name);
         printf("ID: %d\n", ptr->student.id);
         printf("Grades: ");
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++)
+        {
             printf("%d ", ptr->student.grades[i]);
         }
         printf("\n\n");
@@ -187,33 +250,53 @@ void printAll() {
     }
 }
 
-
-
-
-int main() {
+int main()
+{
     int choose = 0;
 
-    do {
+    do
+    {
         printf("\nMenu:\n");
         printf("1: Add Students\n");
-        printf("2: Insert a Student\n");
-        printf("3: Search by Name\n");
-        printf("4: Search by Id\n");
-        printf("5: Print All Students\n");
-        printf("6: Exit\n");
+        printf("2: Delete by location\n");
+        printf("3: Insert a Student\n");
+        printf("4: Search by Name\n");
+        printf("5: Search by Id\n");
+        printf("6: Print All Students\n");
+        printf("7: Exit\n");
         printf("Your choice: ");
         scanf("%d", &choose);
-        switch (choose) {
-        case 1: {
+        switch (choose)
+        {
+        case 1:
+        {
             printf("How many students do you want to add? ");
             int number_students = 0;
             scanf("%d", &number_students);
-            for (int i = 0; i < number_students; i++) {
+            for (int i = 0; i < number_students; i++)
+            {
                 addNode(fill_Student());
             }
             break;
         }
-        case 2: {
+        case 2:
+        {
+            
+            printf("Enter the location to delete the student: ");
+            int loc = 0;
+            scanf("%d", &loc);
+            int flag = deleteNode(loc);
+
+            if ( flag){
+                printf("the node deleted\n");
+            }else{
+                printf("Error");
+            }
+            
+            break;
+        }
+        case 3:
+        {
             printf("Insert the student's details:\n");
             struct Student student = fill_Student();
             printf("Enter the location to insert the student: ");
@@ -222,47 +305,55 @@ int main() {
             insertNode(student, loc);
             break;
         }
-        case 3: {
+        case 4:
+        {
             printf("Enter the name of the student to search for: ");
             char name[20];
             scanf("%s", name);
-            struct Student* student = searchWithName(name);
-            if (student) {
+            struct Student *student = searchWithName(name);
+            if (student)
+            {
                 printf("Name: %s\n", student->name);
                 printf("ID: %d\n", student->id);
                 printf("Grades: %d %d %d\n", student->grades[0], student->grades[1], student->grades[2]);
-            } else {
+            }
+            else
+            {
                 printf("Student not found.\n");
             }
             break;
         }
 
-         case 4: {
+        case 5:
+        {
             printf("Enter the ID of the student to search for: ");
             int id;
             scanf("%d", &id);
-            struct Student* student = searchWitId(id);
-            if (student) {
+            struct Student *student = searchWitId(id);
+            if (student)
+            {
                 printf("Name: %s\n", student->name);
                 printf("ID: %d\n", student->id);
                 printf("Grades: %d %d %d\n", student->grades[0], student->grades[1], student->grades[2]);
-            } else {
+            }
+            else
+            {
                 printf("Student not found.\n");
             }
             break;
         }
-        case 5:
+        case 6:
             printf("All Students:\n");
             printAll();
             break;
-        case 6:
+        case 7:
             printf("Exiting program.\n");
             break;
         default:
             printf("Invalid choice. Please try again.\n");
             break;
         }
-    } while (choose != 6);
+    } while (choose != 7);
 
     freeList();
     return 0;
